5a1c333694ac64fc07fdfccbf2f435dfcb8d55fc
Jamie Cha <chaeunhee98@gmail.com>
2022-12-21 15:16:36 -0600

    configure mvc

--- src/server.js
+++ src/server.js
@@ -8,5 +8,41 @@ const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
 const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
 
 require("dotenv").config({ path: "./src/config/.env" });
+
+require("./src/database/passport")(passport);
+
+connectDB();
+
+app.set("view enginer", "ejs");
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(logger("dev"));
+
+app.use(methodOverride("_method"));
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use("/", mainRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running, you better catch it!");
+});
